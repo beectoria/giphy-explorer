213e7d5975915c7121f28c5cc79d54d292e6c035
Victória Maciel <victoriamacieluni@gmail.com>
2026-07-31 10:46:24 -0300

    fix(giphy): corrige configuração da API e cliente de requisições

--- src/services/giphy/giphyClient.js
+++ src/services/giphy/giphyClient.js
@@ -1,10 +1,15 @@
 import axios from 'axios'
 
 const giphyClient = axios.create({
-  baseURL: '/giphy-api/v1/gifs',
-  params: {
-    api_key: import.meta.env.VITE_GIPHY_API_KEY
+  baseURL: '/giphy-api/v1/gifs'
+})
+
+giphyClient.interceptors.request.use(config => {
+  config.params = {
+    ...config.params,
+    api_key: import.meta.env.QCLI_GIPHY_API_KEY
   }
+  return config
 })
 
 export default giphyClient
